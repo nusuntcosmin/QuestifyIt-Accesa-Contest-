1874d169bd5eaa731c386157b05850ad1aa0508c
Cosmin Popa <cosminpopa645@gmail.com>
2023-04-11 00:45:06 +0300

    Implemented Quest class

--- src/main/java/com/example/questifyit/domain/Quest.java
+++ src/main/java/com/example/questifyit/domain/Quest.java
@@ -1,10 +1,58 @@
 package com.example.questifyit.domain;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 public class Quest implements Entity<UUID> {
+
+    private UUID questId;
+    private String description;
+    private String answer;
+    private Timestamp date;
+    private Boolean solved;
+    private User creator;
+    private Integer tokens;
+
+    public void setSolved(Boolean solved) {
+        this.solved = solved;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public Boolean getSolved() {
+        return solved;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public Integer getTokens() {
+        return tokens;
+    }
+
+    public Quest(UUID questId, String description, String answer, Timestamp date, Boolean solved, User creator, Integer tokens) {
+        this.questId = questId;
+        this.description = description;
+        this.answer = answer;
+        this.date = date;
+        this.solved = solved;
+        this.creator = creator;
+        this.tokens = tokens;
+    }
+
     @Override
     public UUID getEntityID() {
-        return null;
+        return questId;
     }
 }
