1ece1b81ce6831d7f0f7c65e7ea1e5fa7b4a478a
Cosmin Popa <cosminpopa645@gmail.com>
2023-04-11 00:45:59 +0300

    instantiated repositories and services

--- src/main/java/com/example/questifyit/Main.java
+++ src/main/java/com/example/questifyit/Main.java
@@ -1,12 +1,20 @@
 package com.example.questifyit;
 
+import com.example.questifyit.repository.database.factory.RepositoryDbFactory;
+import com.example.questifyit.repository.interfaces.IBadgeRepository;
+import com.example.questifyit.repository.interfaces.IRepository;
+import com.example.questifyit.repository.interfaces.IUserBadgesRepository;
+import com.example.questifyit.repository.interfaces.IUserRepository;
+import com.example.questifyit.service.Service;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileReader;
 import java.util.Objects;
+import java.util.Properties;
 
 public class Main extends Application {
 
@@ -18,9 +26,14 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("intro-transition-view.fxml"));
 
-
         Scene scene = new Scene(fxmlLoader.load(), 648, 402);
+
+        IBadgeRepository badgeRepository = (IBadgeRepository) RepositoryDbFactory.getInstanceOfRepositoryDbFactory().getBadgeRepository();
+        IUserRepository userRepository = (IUserRepository) RepositoryDbFactory.getInstanceOfRepositoryDbFactory().getUserRepository();
+        IUserBadgesRepository userBadgesRepository =(IUserBadgesRepository) RepositoryDbFactory.getInstanceOfRepositoryDbFactory().getUserBadgesRepository();
+        Service service = new Service(userRepository,badgeRepository,null,userBadgesRepository);
         fxmlLoader.<BaseController>getController().setPrimaryStage(primaryStage);
+        fxmlLoader.<BaseController>getController().setService(service);
         primaryStage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("questifyit-symbol-removebg-preview-removebg-preview-removebg-preview-removebg-preview.png"))));
         primaryStage.setTitle("QuestifyIt");
         primaryStage.setScene(scene);
