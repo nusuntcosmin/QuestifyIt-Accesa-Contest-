f933e13577242238e6f7c17e64ec079b6b98f6d4
Cosmin Popa <cosminpopa645@gmail.com>
2023-04-10 20:46:56 +0300

    Pair structure created for primary keys made from 2 columns

---
+++ src/main/java/com/example/questifyit/utils/Pair.java
@@ -0,0 +1,43 @@
+package com.example.questifyit.utils;
+
+import java.util.Objects;
+
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?> pair = (Pair<?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
